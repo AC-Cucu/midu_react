a0a33c1b81d7f083f2ec260b774cb661aab396d8
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-05-28 12:16:42 +0200

    React - ver como funciona .map en los arrays;

---
+++ React/projects/00-repaso-map/funcionMap.js
@@ -0,0 +1,7 @@
+const nuevoArray = arrayOriginal.map((elemento, indice, array) => {
+    //elemento es obligatorio
+    // indice y array son opcionales
+
+    //Lógica de transformación
+    return elementoTransformado
+});
