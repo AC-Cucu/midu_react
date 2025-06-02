8bc1587ba4d76e7a2afdf285cbb042716b8df5b8
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-06-02 14:19:30 +0200

    React - ajustar tabulación constantes;

--- React/projects/02-tic-tac-toe/src/Board.jsx
+++ React/projects/02-tic-tac-toe/src/Board.jsx
@@ -7,21 +7,21 @@ import { WinnerModal } from './components/Winner';
 
 import './App.css'
 
-  const TURNS = {
-    X: "X",
-    O: "O"
-  }
+const TURNS = {
+  X: "X",
+  O: "O"
+}
 
-  const WINNER_COMBOS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function Board() {
   //const board = Array(9).fill(null);
