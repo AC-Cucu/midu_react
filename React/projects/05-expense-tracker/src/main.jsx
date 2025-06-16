ad340ffcaacf18e16cf020fdfe598c2119805932
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-06-16 12:16:38 +0200

    React - Expense Tracker - Añadir el nuevo componente en el render del root;

--- React/projects/05-expense-tracker/src/main.jsx
+++ React/projects/05-expense-tracker/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ExpenseTracker from './components/ExpenseTracker'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>    
+    <>
+      <ExpenseTracker/>
     </>
-  </StrictMode>,
+  </StrictMode>
 )
