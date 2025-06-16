87a18c75bcf413ca459fb351b25eff3ebc6b5e0a
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-06-16 14:16:50 +0200

    React - Expense Tracker - Añadir useEffect para recuperar y guardar los gastos en localStorage;

--- React/projects/05-expense-tracker/src/main.jsx
+++ React/projects/05-expense-tracker/src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ExpenseTracker from './components/ExpenseTracker'
 
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(  
     <>
       <ExpenseTracker/>
     </>
-  </StrictMode>
 )
